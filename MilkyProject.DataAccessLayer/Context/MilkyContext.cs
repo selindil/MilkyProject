aeef09b188b4482f8819e64e2a2a2dfb8547bde3
selindil <0selindil@gmail.com>
2024-11-13 15:48:01 +0300

    ...

---
+++ MilkyProject.DataAccessLayer/Context/MilkyContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using MilkyProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilkyProject.DataAccessLayer.Context
+{
+    public class MilkyContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-F46V4KP\\MSSQLSERVER1;initial Catalog=MilkyDb;integrated Security=true");
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products{ get; set; } 
+        public DbSet<Slider> Sliders { get; set; }
+    }
+}
