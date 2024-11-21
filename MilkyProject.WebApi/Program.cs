bc37234858bebad7b0d1c25582c0ff84bc678425
selindil <0selindil@gmail.com>
2024-11-21 17:59:07 +0300

    ...

--- MilkyProject.WebApi/Program.cs
+++ MilkyProject.WebApi/Program.cs
@@ -1,6 +1,24 @@
+using MilkyProject.BusinessLayer.Abstract;
+using MilkyProject.BusinessLayer.Concrete;
+using MilkyProject.DataAccessLayer.Abstract;
+using MilkyProject.DataAccessLayer.Context;
+using MilkyProject.DataAccessLayer.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<ICategoryService, CategoryManager>();
+builder.Services.AddScoped<ICategoryDal, EfCategoryDal>();
+
+builder.Services.AddScoped<ISliderService, SliderManager>();
+builder.Services.AddScoped<ISliderDal, EfSliderDal>();
+
+builder.Services.AddScoped<IProductService, ProductManager>();
+
+
+builder.Services.AddScoped<IProductDal, EfProductDal>();
+
+builder.Services.AddDbContext<MilkyContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
